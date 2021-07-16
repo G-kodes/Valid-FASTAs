8331897b6231c415babacfc18f93b9f758f04ab8
Graeme Ford <frd.graeme@gmail.com>
2021-07-16 22:50:06 +0200

    Raised issue with SNakemake and stopgapped for now

--- workflow/rules/common.py
+++ workflow/rules/common.py
@@ -1,3 +1,8 @@
+__author__ = "Graeme Ford"
+__copyright__ = "Copyright 2021, Graeme Ford"
+__email__ = "graeme.ford@tuks.co.za"
+__license__ = "GNUv3"
+
 import os
 import json
 import re
@@ -31,7 +36,7 @@ def GetInputFile(wildcards: object = dict()) -> str:
             4) == wildcards.ext and re.search(reX, file).group(3) == wildcards.filename) for dataset in config["Data"])
     except Exception:
         item = ""
-    print("Error: No Match Found for this input request. FILENAME: " +
+    print("Error: No Match Found for this input request. FILENAME: results/" +
           wildcards.filename + wildcards.ext)
     return item
 
