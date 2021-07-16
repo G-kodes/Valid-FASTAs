8f1d4647dc1c70c845098d053c2b16879d72673a
Graeme Ford <graeme.ford@tuks.co.za>
2021-07-16 15:22:03 +0200

    Updated input functions

--- workflow/rules/common.py
+++ workflow/rules/common.py
@@ -28,7 +28,7 @@ def GetInputFile(wildcards: object = dict()) -> str:
         str(wildcard)
     try:
         item = next(item["Path"]
-                    for item in config["Data"] if item["Name"] == wildcard)
+                    for item in config["Data"] if item["Name"] == wildcard.name)
     except StopIteration:
         pass
     print("File to fetch as input:", item)
